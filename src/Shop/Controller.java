eaf1cb08987a6f475c906d06951d792f2af22ed8
Tahir raki <rihatax@gmail.com>
2019-04-11 16:23:44 +0200

    Changed

---
+++ src/Shop/Controller.java
@@ -0,0 +1,36 @@
+package Shop;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+
+public class Controller implements Initializable {
+    @FXML
+    private ImageView image1;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button loginButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void displayImage(){
+
+
+    }
+}
